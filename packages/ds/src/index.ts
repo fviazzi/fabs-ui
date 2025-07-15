f78c21ed4517bf84ca8e84aa271b5fad92157d8c
Fabricio Viazzi <fabricioviazzi@gmail.com>
2025-07-14 22:32:34 -0300

    feat(#5): add initial storybook implementation

--- packages/ds/src/index.ts
+++ packages/ds/src/index.ts
@@ -1,1 +1,4 @@
+import './tailwind.css';
+
 export { Button } from "./components/atoms/button/button";
+export { DsProvider } from "./core/DsProvider";
--- packages/ds/vite.config.ts
+++ packages/ds/vite.config.ts
@@ -1,14 +1,50 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+/// <reference types="vitest/config" />
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dts from 'vite-plugin-dts'
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 
-// https://vite.dev/config/
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({ outDir: 'dist', insertTypesEntry: true })
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+  build: {
+    lib: { entry: 'src/index.ts', formats: ['es'], fileName: 'index' },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
     },
   },
-})
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: 'playwright',
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
+  }
+});
