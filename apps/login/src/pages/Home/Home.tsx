732df05f1a24b3467fd256cade084f7d2dd17819
Fabricio Viazzi <fabricioviazzi@gmail.com>
2025-07-18 23:57:46 -0300

    feat(#6): Add sample consumer app

---
+++ apps/login/src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@fabs-ui/ds";
+
+export const Home = () => (
+  <>
+    <h2>Home</h2>
+    <hr />
+    <Button variant="destructive" size="lg">Button from DS</Button>
+  </>
+);
--- apps/login/vite.config.ts
+++ apps/login/vite.config.ts
@@ -1,15 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
   build: {
     rollupOptions: {
       input: 'src/index.tsx',
